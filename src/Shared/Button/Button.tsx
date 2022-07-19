49bf640ca04dd303f3640bb912754ba5e91b8cb4
ArafatSadi1 <arafatsadi1@gmail.com>
2022-07-19 15:16:27 +0600

    create button

--- src/Shared/Button/Button.tsx
+++ src/Shared/Button/Button.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Button = () => {
+type ButtonProps = {
+    children: JSX.Element | JSX.Element[];
+  }
+
+const Button = ({children}: ButtonProps) => {
     return (
-        <div>
-            
-        </div>
+        <button className='bg-primary py-2 px-4 text-white font-semibold rounded hover:bg-[#01aba8] duration-300'>
+            {children}
+        </button>
     );
 };
 
