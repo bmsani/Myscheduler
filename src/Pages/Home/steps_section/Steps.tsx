b8a7a7ec68ff62d0206363f3e4c1823cebc88264
unknown <mdtanvir6070@gmail.com>
2022-07-21 13:39:16 +0600

    steps responsive done

--- src/Pages/Home/steps_section/Steps.tsx
+++ src/Pages/Home/steps_section/Steps.tsx
@@ -3,15 +3,15 @@ import React from "react";
 const Steps = () => {
   return (
     <div className="mt-20">
-      <h2 className="text-primary text-3xl lg:text-5xl font-bold w-full lg:w-[750px] text-center mx-auto pb-16 px-2">
+      <h2 className="text-primary text-2xl md:text-3xl lg:text-4xl font-bold w-full lg:w-[750px] text-center mx-auto pb-16 px-2">
         We make it easy to get started
       </h2>
-      <ul className="steps steps-vertical lg:steps-horizontal w-full">
+      <ul className="steps steps-vertical md:steps-horizontal w-full pb-4">
         <li className="step step-neutral">
-          <div className="card w-64 lg:w-96 text-neutral border shadow-lg">
+          <div className="card w-64 lg:w-full text-neutral border shadow-lg">
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-2xl font-semibold">Create simple rules</h2>
-              <p className="text-lg text-gray-600">
+              <h2 className="card-title text-xl md:text-2xl font-bold">Create simple rules</h2>
+              <p className="text-sm md:text-lg text-gray-600 text-justify">
                 Let MyScheduler know your availability preferences and it'll do
                 the work for you.
               </p>
@@ -19,18 +19,18 @@ const Steps = () => {
           </div>
         </li>
         <li className="step step-neutral">
-          <div className="card w-64 lg:w-96 h-full text-neutral border shadow-lg">
+          <div className="card w-64 lg:w-full h-full text-neutral border shadow-lg">
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-2xl font-semibold">Share your link</h2>
-              <p className="text-lg text-gray-600">Send guests your Calendly link or embed it on your website.</p>
+              <h2 className="card-title text-xl md:text-2xl font-bold">Share your link</h2>
+              <p className="text-sm md:text-lg text-gray-600 text-justify">Send guests your MyScheduler link or embed it on your website.</p>
             </div>
           </div>
         </li>
         <li className="step step-neutral">
-          <div className="card w-64 lg:w-96 h-full text-neutral border shadow-lg">
+          <div className="card w-64 lg:w-full h-full text-neutral border shadow-lg">
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-2xl font-semibold">Get booked</h2>
-              <p className="text-lg text-gray-600">They pick a time and the event is added to your calendar.</p>
+              <h2 className="card-title text-xl md:text-2xl font-bold">Get booked</h2>
+              <p className="text-sm md:text-lg text-gray-600 text-justify">They pick a time and the event is added to your calendar.</p>
             </div>
           </div>
         </li>
