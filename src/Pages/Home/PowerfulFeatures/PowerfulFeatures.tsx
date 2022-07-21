1f4e4be5eaefadc371edfe411ea2bf12b2245ede
sabbir10s <sabbirahmed1023@gmail.com>
2022-07-21 17:11:47 +0600

    home desin update

--- src/Pages/Home/PowerfulFeatures/PowerfulFeatures.tsx
+++ src/Pages/Home/PowerfulFeatures/PowerfulFeatures.tsx
@@ -1,20 +1,23 @@
+import img1 from "../../../Utilities/Image/image-1.jpg";
+import img2 from "../../../Utilities/Image/image-3.jpg";
+import img3 from "../../../Utilities/Image/image-2.jpg";
 const PowerfulFeatures = () => {
   const cardData = [
     {
-      id: '1',
-      icon: 'https://img.freepik.com/free-vector/alert-concept-illustration_114360-238.jpg?t=st=1658382151~exp=1658382751~hmac=4ac206dffa9aa573ea315b73443fc6b7f4351ff0752424db8ae8f2cc9a69f131&w=740',
+      id: "1",
+      icon: img1,
       heading: "Reduce no shows with reminders",
       para: "Automatic reminders and notifications significantly reduce no shows and admin work.",
     },
     {
-      id: '2',
-      icon: 'https://i.insider.com/606b61e6856cd700198a33a0?width=1136&format=jpeg&fbclid=IwAR116spquUnSpWLUxdMmnQ3Eob7wEG8d4fUf_4kUgY4yYZiIr2H6Ky30Gug',
+      id: "2",
+      icon: img2,
       heading: "Video/Virtual meeting links",
       para: "Automatically create unique Zoom, Google Meet, and MS Teams links for meetings scheduled.",
     },
     {
-      id: '3',
-      icon: 'https://img.freepik.com/free-vector/flat-hand-drawn-time-management-illustration_23-2148829093.jpg',
+      id: "3",
+      icon: img3,
       heading: "Custom available and control",
       para: "Set up custom availability, padding between appointments and appointment duration.",
     },
@@ -22,16 +25,17 @@ const PowerfulFeatures = () => {
   return (
     <section className="w-full text-center mt-20 px-10">
       <div className="mb-8">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primary">Powerful features</h1>
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primary">
+          Powerful features
+        </h1>
         <p className="font-medium text-justify md:text-center mx-10 md:mx-0 md:text-lg">
           Simple, easy to use features to help automate scheduling with your
           customers.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {cardData.map((data) => (
-          <PowerCard data={data}
-            key={data.id } />
+          <PowerCard data={data} key={data.id} />
         ))}
       </div>
     </section>
@@ -40,12 +44,10 @@ const PowerfulFeatures = () => {
 
 const PowerCard = ({ data }: any) => {
   return (
-    <div className="hover:bg-white text-center px-4 py-6 rounded">
-      <img className="mx-auto w-40" src={data.icon} alt="" />
-      <p className="font-bold text-xl md:text-2xl bg-transparent">{data.heading}</p>
-      <p className="text-sm md:text-lg text-justify bg-transparent">
-        {data.para}
-      </p>
+    <div className="p-4 text-center hover:shadow-lg duration-500">
+      <img className="max-w-[250px] mx-auto" src={data.icon} alt="" />
+      <p className="font-bold text-xl md:text-xl">{data.heading}</p>
+      <p className="text-sm text-slate-500">{data.para}</p>
     </div>
   );
 };
