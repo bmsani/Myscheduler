f56fecc2f782242fbc368062714b8c52cc61850d
ArafatSadi1 <arafatsadi1@gmail.com>
2022-07-24 17:58:34 +0600

    add github logo

--- src/Shared/SocialLogin/SocialLogin.tsx
+++ src/Shared/SocialLogin/SocialLogin.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import googleLogo from "../../Utilities/Logos/Google.png";
-import facebookLogo from "../../Utilities/Logos/Facebook.png";
+import githubLogo from "../../Utilities/Logos/github.png";
 const SocialLogin = ({signInWithGoogle}: any) => {
   return (
     <div className="flex justify-center gap-5">
@@ -8,7 +8,7 @@ const SocialLogin = ({signInWithGoogle}: any) => {
         <img className="w-[30px]" src={googleLogo} alt="" />
       </button>
       <button className="bg-slate-200 border border-secondary hover:border-primary p-2 rounded-lg hover:shadow duration-300">
-        <img className="w-[30px]" src={facebookLogo} alt="" />
+        <img className="w-[30px]" src={githubLogo} alt="" />
       </button>
     </div>
   );
