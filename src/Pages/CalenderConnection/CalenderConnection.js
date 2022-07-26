dc3cde2c31558c27336e53a49bc9eb8429e233c8
zayanahmed925 <rjraju925@gmail.com>
2022-07-26 18:23:22 +0600

    calender connection added

--- src/Pages/CalenderConnection/CalenderConnection.js
+++ src/Pages/CalenderConnection/CalenderConnection.js
@@ -1,9 +1,102 @@
 import React from 'react';
-
+import logo from '../../Utilities/icon/class_icon.png'
+import calender from '../../Utilities/icon/calendarLogo.png'
+import arrowRight from '../../Utilities/icon/calenderArrow.png'
+import arrowLeft from '../../Utilities/icon/calederArrow2.png'
 const CalenderConnection = () => {
+
     return (
         <div>
             <h2>Calender connection</h2>
+            <div className='shadow border container mx-auto pb-5'>
+                <div className='flex justify-between py-4 px-8'>
+                    <h2 className='text-xl'>My calender Account</h2>
+                    <button className='text-sm  rounded-lg btn btn-sm btn-outline btn-info'>Add Calender Account</button>
+                </div>
+                <div className=''>
+                    <div className='flex justify-between border-t border-b px-8 py-8 bg-gray-50'>
+                        <div className='flex'>
+                            <div class="avatar">
+                                <div class="w-14 p-3 shadow rounded-full border">
+                                    <img src={calender} alt='' />
+                                </div>
+                            </div>
+                            <div className='ml-4'>
+                                <h2 className='text-light text-sm'>Google</h2>
+                                <h2>zehadul925@gmail.com</h2>
+                            </div>
+                        </div>
+                        <div>
+                            <button className='text-secondary text-sm hover:text-red-500 hover:underline underline-offset-1'>Disconnect</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className='shadow border container mx-auto mt-8 mb-20'>
+                <div className='flex justify-between py-4 px-8 border-b'>
+                    <h2 className='text-xl'>Configuration</h2>
+                </div>
+                <div className='py-8 px-8'>
+                    <div className='grid grid-cols-2 '>
+                        <div className='flex '>
+                            <div>
+                                <img src={arrowRight} className="w-[34px]" alt="" />
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='font-bold text-lg'>Check for conflict</h1>
+                                <h2 className='text-light text-xs'>Set the calendar(s) to check for conflicts to prevent double bookings.
+                                </h2>
+                            </div>
+                        </div>
+                        <div className='flex justify-between border hover:border-blue-800 p-4'>
+                            <div className='flex '>
+                                <div class="avatar">
+                                    <div class="w-11 p-3 shadow border rounded-full">
+                                        <img src={calender} alt='' />
+                                    </div>
+                                </div>
+
+                                <div className='ml-4'>
+                                    <h1>Check zehadul925@gmail.com</h1>
+                                    <li className='list-disc'>zehadul925@gmail.com</li>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='text-blue-800 text-sm'>Edit</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border my-6'></div>
+                    <div className='grid grid-cols-2 '>
+                        <div className='flex '>
+                            <div>
+                                <img src={arrowLeft} className="w-[34px]" alt="" />
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='font-bold text-lg'>Check for conflict</h1>
+                                <h2 className='text-light text-xs'>Set the calendar you would like to add new events to as they’re scheduled.</h2>
+                            </div>
+                        </div>
+                        <div className='flex justify-between border hover:border-blue-800 p-4'>
+                            <div className='flex '>
+                                <div class="avatar">
+                                    <div class="w-11 p-3 shadow border rounded-full">
+                                        <img src={calender} alt='' />
+                                    </div>
+                                </div>
+                                <div className='ml-4'>
+                                    <h1>Check zehadul925@gmail.com</h1>
+                                    <li className='list-disc'>zehadul925@gmail.com</li>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='text-blue-800 text-sm'>Edit</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
