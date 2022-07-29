3a79e14d383347955b3aa0a00f7e4dc19f7eef17
unknown <mdtanvir6070@gmail.com>
2022-07-29 11:52:58 +0600

    Responsive setup

--- src/Pages/Types/Types.tsx
+++ src/Pages/Types/Types.tsx
@@ -14,12 +14,16 @@ const Types = () => {
         <div>
           <div>
             <div className="hero h-screen bg-base-100">
-              <div className="hero-content flex-col lg:flex-row-reverse gap-x-20">
+              <div className="hero-content grid grid-cols-1 md:grid-cols-2 gap-10">
                 <img src={oneOnOne} className="w-[650px] rounded-lg" alt="" />
                 <div>
-                  <h1 className="text-5xl font-bold">Meeting with</h1>
-                  <h1 className="text-5xl font-bold">One on one</h1>
-                  <p className="text-xl text-justify">
+                  <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                    Meeting with
+                  </h1>
+                  <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                    One on one
+                  </h1>
+                  <p className="text-sm md:text-md text-justify">
                     MyScheduler makes it easy to work smarter when you’re
                     working solo. Meetings, sessions, and appointments become
                     more efficient ways to achieve success and accomplish goals.
@@ -28,18 +32,18 @@ const Types = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center">
-            <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center px-5 md:px-10">
+            <div className="md:order-2">
               <img src={assistant} alt="" />
             </div>
-            <div>
-              <h6 className="text-2xl font-bold">
+            <div className="md:order-1">
+              <h6 className="text-sm lg:text-xl font-bold">
                 AUTOMATED NOTIFICATIONS & FOLLOW-UPS
               </h6>
-              <h3 className="text-5xl font-bold my-3">
+              <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold my-3">
                 Work like you have a personal assistant
               </h3>
-              <p className="text-xl text-justify">
+              <p className="text-sm md:text-md text-justify">
                 Because MyScheduler automates administrative tasks like sending
                 reminder emails and follow-ups, you can focus on the work that
                 builds your business and brings customers back for more.
@@ -50,12 +54,16 @@ const Types = () => {
         <div>
           <div>
             <div className="hero h-screen bg-base-100">
-              <div className="hero-content flex-col lg:flex-row-reverse gap-x-20">
+              <div className="hero-content grid grid-cols-1 md:grid-cols-2 gap-10">
                 <img src={team} className="w-[650px] rounded-lg" alt="" />
                 <div>
-                  <h1 className="text-5xl font-bold">Meeting with</h1>
-                  <h1 className="text-5xl font-bold">Your team</h1>
-                  <p className="text-xl text-justify">
+                  <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                    Meeting with
+                  </h1>
+                  <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                    Your team
+                  </h1>
+                  <p className="text-sm md:text-md text-justify">
                     Whether your team’s performance is measured by sales
                     revenue, recruiting pipeline, or customer retention,
                     scheduling automation enables your team to hit goals faster.
@@ -64,16 +72,16 @@ const Types = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center">
-            <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center px-5 md:px-10">
+            <div className="md:order-2">
               <img src={scheduling} alt="" />
             </div>
-            <div>
+            <div className="md:order-1">
               <h6 className="text-2xl font-bold">TIME MANAGEMENT</h6>
-              <h3 className="text-5xl font-bold my-3">
+              <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold my-3">
                 Automate scheduling, improve team performance
               </h3>
-              <p className="text-xl text-justify">
+              <p className="text-sm md:text-md text-justify">
                 Scheduling automation eliminates time-consuming admin tasks so
                 your team can focus on higher priorities. With actionable
                 insights into your team’s scheduling activities and integrations
@@ -88,13 +96,19 @@ const Types = () => {
           <div>
             <div>
               <div className="hero h-screen bg-base-100">
-                <div className="hero-content flex-col lg:flex-row-reverse gap-x-20">
+                <div className="hero-content grid grid-cols-1 md:grid-cols-2 gap-10">
                   <img src={group} className="w-[650px] rounded-lg" alt="" />
                   <div>
-                    <h1 className="text-5xl font-bold">Meeting with</h1>
-                    <h1 className="text-5xl font-bold">Your big</h1>
-                    <h1 className="text-5xl font-bold">Community</h1>
-                    <p className="text-xl text-justify">
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                      Meeting with
+                    </h1>
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                      Your big
+                    </h1>
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+                      Community
+                    </h1>
+                    <p className="text-sm md:text-md text-justify">
                       Meetings are the lifeblood of high-performing teams
                     </p>
                   </div>
@@ -102,26 +116,26 @@ const Types = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center">
-            <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center items-center px-5 md:px-10">
+            <div className="md:order-2">
               <img src={GroupMeeting} alt="" />
             </div>
-            <div>
-              <h3 className="text-5xl font-bold my-3">
+            <div className="md:order-1">
+              <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold my-3">
                 Enterprise-grade security and management
               </h3>
-              <p className="text-xl text-justify">
+              <p className="text-sm md:text-md text-justify">
                 Centralized team management: Manage users, teams, reporting, and
                 shared event types from one centralized admin dashboard.
               </p>
 
-              <p className="text-xl text-justify">
+              <p className="text-sm md:text-md text-justify">
                 Secure and compliant: Securely deploy MyScheduler with SAML SSO
                 and SCIM provisioning. GDPR compliant and SOC 2 Type II
                 certified.
               </p>
 
-              <p className="text-xl text-justify">
+              <p className="text-sm md:text-md text-justify">
                 Complete data privacy: MyScheduler never stores passwords,
                 content, or PII associated with your team’s accounts.
               </p>
