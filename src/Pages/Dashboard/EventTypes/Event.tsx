bf2392a43be3cbc91309c902eb0a82d2712c1a68
unknown <mdtanvir6070@gmail.com>
2022-08-10 22:48:34 +0600

    event get successfull

--- src/Pages/Dashboard/EventTypes/Event.tsx
+++ src/Pages/Dashboard/EventTypes/Event.tsx
@@ -1,19 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../../init.firebase";
+import Loading from "../../../Shared/LoadingSpinner/Loading";
 import userImg from "../../../Utilities/icon/profile.png";
 const Event = () => {
   const [user] = useAuthState(auth);
 
   const link = user?.email;
+  const { data: events, isLoading } = useQuery(["events", link], () =>
+    fetch(`http://localhost:5000/getEvent/${link}`).then((res) => res.json())
+  );
 
-  // const handleCreateEvent = () => {
-  //   const url =
-  //     "http://localhost:3001/";
-  //   window.open(url, "_blank");
-  // }
-
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div className="mr-10 ml-5 pt-12">
       <div className="flex flex-col md:flex-row items-center justify-between">
@@ -21,7 +23,6 @@ const Event = () => {
           <img className="w-[70px] rounded-full" src={userImg} alt="" />
           <div>
             <p>Admin name</p>
-            {/* <p className="text-secondary">Admin user</p> */}
             <Link to="/eventBooking">
               <p className="text-secondary">{link?.split("@")[0]}</p>
             </Link>
@@ -35,17 +36,24 @@ const Event = () => {
       </div>
       <div className=" divider"></div>
 
-      <div className="cart border rounded-2xl w-[320] md:w-[350px] shadow hover:shadow-xl duration-300 cursor-pointer">
-        <div className="bg-blue-500 h-2 w-full rounded-t-2xl"></div>
-        <div className="p-5">
-          <h2 className="text-xl">Business Meeting</h2>
-          <p className="text-sm">30 mins, One-on-One</p>
-          <p className="text-secondary mt-2">View booking page</p>
-          <div className="divider"></div>
-          <button className="mt-4 py-1 px-4 border border-primary rounded-full text-primary hover:shadow-md hover:shadow-gray-500 duration-300 cursor-pointer">
-            Share
-          </button>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {events.map((e: any) => (
+          <div
+            className="cart border rounded-2xl w-[320] md:w-[350px] shadow hover:shadow-xl duration-300 cursor-pointer"
+            key={e._id}
+          >
+            <div className="bg-blue-500 h-2 w-full rounded-t-2xl"></div>
+            <div className="p-5">
+              <h2 className="text-xl">{e.eventName}</h2>
+              <p className="text-sm">{e.eventDuration} mins, One-on-One</p>
+              <p className="text-secondary mt-2">View booking page</p>
+              <div className="divider"></div>
+              <button className="mt-4 py-1 px-4 border border-primary rounded-full text-primary hover:shadow-md hover:shadow-gray-500 duration-300 cursor-pointer">
+                Share
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
